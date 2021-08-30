75169535ed72c82fcc2e009a8ec65ea30ba86d6b
Russel <russel1234561@gmail.com>
2021-08-30 16:31:54 +0200

    Created header section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,40 @@
+import Menu from "../menu/Menu";
+import "./header.css";
+import phone from "../../sources/icons/phone.svg";
+import user from "../../sources/icons/user.svg";
+import search from "../../sources/icons/search.svg";
+
 const Header = () => {
   return (
-    <header>
-      <p>header</p>
+    <header className="header">
+      <Menu />
+      <section className="header__right">
+        <span className="header__right__text">CENTRUM OBSŁUGI KLIENTA</span>
+        <a href="tel:800 080 800" className="header__right__link">
+          <img
+            src={phone}
+            alt="phone icon"
+            className="header__right__link__image"
+          ></img>{" "}
+          800 090 800
+        </a>
+        <span className="header__right__text">
+          <img
+            src={search}
+            alt="phone icon"
+            className="header__right__link__image"
+          ></img>{" "}
+          800 090 800
+        </span>
+        <span className="header__right__text">
+          <img
+            src={user}
+            alt="phone icon"
+            className="header__right__link__image"
+          ></img>{" "}
+          800 090 800
+        </span>
+      </section>
     </header>
   );
 };
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,15 @@
+import "./menu.css";
+
+const Menu = () => {
+  return (
+    <section className="header__hamburgerMenu">
+      <section className="header__hamburgerMenu__line"></section>
+      <section className="header__hamburgerMenu__line">
+        <section className="header__hamburgerMenu__line__inside"></section>
+      </section>
+      <section className="header__hamburgerMenu__line"></section>
+    </section>
+  );
+};
+
+export default Menu;
