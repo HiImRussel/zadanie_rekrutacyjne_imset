8f74d9f831b46f903cbba7478eed0365e865e8b6
Russel <russel1234561@gmail.com>
2021-08-31 15:01:31 +0200

    Added gsap and open/close menu

--- src/components/header/menu/Menu.js
+++ src/components/header/menu/Menu.js
@@ -1,14 +1,72 @@
 import "./menu.css";
+import { gsap } from "gsap";
+import { useState } from "react";
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    if (isOpen === false) {
+      gsap.fromTo(
+        "#mobileMenu",
+        { opacity: 0 },
+        { opacity: 1, visibility: "inherit" }
+      );
+      gsap.fromTo(".mobileMenu__nav", { width: "0px" }, { width: "30%" });
+      setIsOpen((prevValue) => !prevValue);
+    } else {
+      gsap.fromTo(
+        "#mobileMenu",
+        { opacity: 1 },
+        {
+          opacity: 0,
+          onComplete: () => {
+            gsap.to("#mobileMenu", { visibility: "hidden" });
+          },
+        }
+      );
+      gsap.fromTo(".mobileMenu__nav", { width: "30%" }, { width: "0px" });
+      setIsOpen((prevValue) => !prevValue);
+    }
+  };
   return (
-    <section className="header__hamburgerMenu">
-      <section className="header__hamburgerMenu__line"></section>
-      <section className="header__hamburgerMenu__line">
-        <section className="header__hamburgerMenu__line__inside"></section>
+    <>
+      <section className="header__hamburgerMenu" onClick={handleClick}>
+        <section className="header__hamburgerMenu__line"></section>
+        <section className="header__hamburgerMenu__line">
+          <section className="header__hamburgerMenu__line__inside"></section>
+        </section>
+        <section className="header__hamburgerMenu__line"></section>
       </section>
-      <section className="header__hamburgerMenu__line"></section>
-    </section>
+      <aside id="mobileMenu">
+        <nav className="mobileMenu__nav">
+          <button className="mobileMenu__nav__btn" onClick={handleClick}>
+            X
+          </button>
+          <ul className="mobileMenu__nav__list">
+            <li className="mobileMenu__nav__list__element">
+              <a href="...." className="mobileMenu__nav__link">
+                O NAS
+              </a>
+            </li>
+            <li className="mobileMenu__nav__list__element">
+              <a href="...." className="mobileMenu__nav__link">
+                KONTAKT
+              </a>
+            </li>
+            <li className="mobileMenu__nav__list__element">
+              <a href="...." className="mobileMenu__nav__link">
+                KARIERA
+              </a>
+            </li>
+            <li className="mobileMenu__nav__list__element">
+              <a href="...." className="mobileMenu__nav__link">
+                DLA MEDIÓW
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
   );
 };
 
